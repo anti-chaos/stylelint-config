c9bdc995095c7b6c693259ad1af75f0b7084c4be
Ourai Lin <ourairyu@gmail.com>
2021-03-11 09:53:56 +0800

    fix: dependency name

--- rules/quality.js
+++ rules/quality.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: ['stylelint-config-recommended'],
   rules: {
     'selector-class-pattern': null,
     'selector-no-qualifying-type': [true, { ignore: ['class'] }],
